890b2e58717941487fe495e30a25a04edc10066f
priyap10 <priyapandey.01102005@gmail.com>
2025-07-25 18:50:04 +0530

    Update insertion_sort.cpp

--- SORT-1/insertion_sort.cpp
+++ SORT-1/insertion_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void insertion_sort(int arr[],int n){
-	for(int=0;i<=n-1;i++){
+	for(int i=0;i<=n-1;i++){
 		int j=i;
 		while(j>0 && arr[j-1]>arr[j]){
 			int temp=arr[j-1];
